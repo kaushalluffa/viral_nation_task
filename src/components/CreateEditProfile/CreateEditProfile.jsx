11ded55e4a41fa0d9b70f0cb6d7c9dd46781b546
kaushalluffa <webdevelopment852@gmail.com>
2023-03-01 11:08:19 -0500

    handleClose func added to close btn on edit profile comp

--- src/components/CreateEditProfile/CreateEditProfile.jsx
+++ src/components/CreateEditProfile/CreateEditProfile.jsx
@@ -77,7 +77,7 @@ const CreateEditProfile = () => {
         >
           <Typography variant="h5">Create Profile</Typography>
           <IconButton>
-            <CloseIcon />
+            <CloseIcon onClick={handleClose}/>
           </IconButton>
         </DialogTitle>
         <Divider />
